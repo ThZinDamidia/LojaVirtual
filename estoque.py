9f6618b140f51f15fa46f8baf15a6e75d199f406
Someone <vortexcpx@gmail.com>
2026-02-24 15:20:46 +0000

    estoque
    Added product listings for various brands including Acer, Apple, and ASUS, along with components.

---
+++ estoque.py
@@ -0,0 +1,71 @@
+ ==============================
+# ESTOQUE DA LOJA
+# ==============================
+
+# Cada produto = (nome, preço)
+
+acer_trabalho = [
+    ("Notebook Acer Aspire AG15 AMD R7 32GB 1TB SSD 15.6", 717),
+    ("Notebook Acer Aspire A16 Snapdragon 16GB 512GB SSD", 620),
+    ("Notebook Acer Aspire Lite i3 8GB 512GB SSD", 497),
+    ("Acer Extensa i7 16GB 512GB SSD", 629),
+    ("Acer Extensa i5 8GB 512GB SSD", 476),
+    ("Acer Ryzen 5 16GB 512GB SSD", 493),
+    ("Acer Ryzen 5 8GB 512GB SSD", 463)
+]
+
+acer_gamer = [
+    ("Acer Nitro V i9 32GB 1TB RTX 5060", 1318),
+    ("Acer Nitro V Ryzen 7 32GB 1TB RTX 5060", 1019),
+    ("Acer Nitro V Ryzen 7 RTX 5050", 1064),
+    ("Acer Aspire 3 i7 32GB 512GB", 808),
+    ("Acer Predator Helios RTX 5070", 1983),
+    ("Acer Aspire 14 Ultra 7", 1115)
+]
+
+apple_trabalho = [
+    ("MacBook Air M2 16GB 256GB", 1024),
+    ("MacBook Air M4 16GB 512GB", 1249),
+    ("MacBook Air M4 16GB 256GB", 999),
+    ("MacBook Air 15 M4 16GB", 1289),
+    ("MacBook Pro M4 Pro 24GB", 2159),
+    ("Mac Mini M4 16GB", 882),
+    ("Mac Mini M4 Pro 24GB", 1557)
+]
+
+apple_gamer = [
+    ("MacBook Pro M5 16GB 1TB", 1889),
+    ("MacBook Pro M5 16GB 512GB", 1699),
+    ("MacBook Pro M5 24GB 1TB", 2349),
+    ("MacBook Air M4 16GB", 999),
+    ("MacBook Air M4 24GB 512GB", 1472)
+]
+
+asus_trabalho = [
+    ("ASUS Vivobook Ryzen 7 16GB 512GB", 599),
+    ("ASUS Vivobook Ryzen 7 16GB 1TB", 699),
+    ("ASUS Vivobook OLED Ryzen 7", 899),
+    ("ASUS Vivobook S Ryzen AI 32GB", 1299),
+    ("ASUS Zenbook Snapdragon 32GB", 1299),
+    ("ASUS Vivobook i7 16GB 1TB", 699)
+]
+
+asus_gamer = [
+    ("ASUS V16 RTX 5060", 1199),
+    ("ASUS ROG Strix G16 RTX 5070", 1828),
+    ("ASUS TUF Gaming RTX 5060", 1553),
+    ("ASUS TUF Gaming A18 RTX 5070", 1584),
+    ("ASUS ROG Zephyrus RTX 5070", 2299),
+    ("ASUS ROG Strix SCAR RTX 5090", 4857)
+]
+
+componentes = [
+    ("Memória RAM 16GB DDR4", 159),
+    ("Memória RAM 64GB DDR4", 505),
+    ("SSD 1TB NVMe", 160),
+    ("SSD 2TB NVMe", 295),
+    ("Placa de Vídeo RTX 5070", 758),
+    ("Placa de Vídeo RTX 5090", 5022),
+    ("Motherboard ASUS PRIME", 67),
+    ("Motherboard ASUS ROG", 187)
+]
